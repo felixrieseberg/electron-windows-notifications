7c4269512fd3d0ed97968e1d60b19a71ec8bfed5
Rita Zhang <rita.z.zhang@gmail.com>
2016-09-27 14:56:13 -0700

    :zap: Customize your own templates

---
+++ samples/template.js
@@ -0,0 +1,32 @@
+// Create shortcut, just to be sure
+require('./shortcut')
+
+// Sample
+const appId = 'electron-windows-notifications'
+const Notification = require('../src/notification')
+const Template = require('../src/template')
+
+let notificationSimple = new Notification({
+    appId: appId,
+    template: new Template({
+    	templateText: '<text id="1">%s</text>'
+    }).getXML(),
+    strings: ['Hi!']
+})
+
+notificationSimple.on('activated', () => console.log('Actived!'))
+notificationSimple.show()
+
+let imagePath = (`file:///${path.join(__dirname, 'images', 'bp.png').replace(/\\/g, '/')}`)
+let notificationImage = new Notification({
+    appId: appId,
+    template: new Template({
+    	templateText: '<text id="1">%s</text>',
+    	templateImage: '<image id="1" src="%s"/>',
+    	templateActions: '<actions><action content="check" arguments="check" /></actions>'
+    }).getXML(),`,
+    strings: [imagePath, 'Hi!']
+})
+
+notificationImage.on('activated', () => console.log('Actived!'))
+notificationImage.show()
