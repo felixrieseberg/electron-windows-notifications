7de5619898b495acde48ebd909af5f2633193457
Nadav Bar <nabar@microsoft.com>
2016-09-29 12:32:31 -0700

    Added different notifications types to electron sample

--- samples/electron/window.js
+++ samples/electron/window.js
@@ -1,52 +1,64 @@
-var path = require('path');
-const Notification = require('electron-windows-notifications').Notification;
-const Template = require('electron-windows-notifications').Template;
+var path = require('path')
+const Notification = require('electron-windows-notifications').Notification
+const Template = require('electron-windows-notifications').Template
 
-var nts = require('@nodert-win10/windows.ui.notifications');
-
-var options = [
-  {
-    title: "Basic Notification",
-    body: "Short message part"
-  },
-  {
-    title: "Content-Image Notification",
-    body: "Short message plus a custom content image",
-    icon: path.join(__dirname, 'icon.png')
-  }
-]
-
-const iconPath = path.join(__dirname, 'icon.png');
+const iconPath = path.join(__dirname, 'icon.png')
 const appId = 'electron-windows-notifications-sample'
 
-var basicTemplate = `<toast><visual><binding template="ToastText01"><text id="1">%s</text></binding></visual></toast>`
-var strings = ['Hi!']
-
 function doNotify(evt) {
+  let template
+  let strings
+  
+  // set notification content accordingly
   if (evt.srcElement.id == "basic") {
-	let notification = new Notification({
+	template = new Template({ 
+	  templateText: '<text>%s</text>'
+	})
+	strings = ['Hi from Electron']
+  }
+  else if (evt.srcElement.id == "image") {
+	template = new Template ({ 
+	  templateText: '<text>%s</text>',
+      templateImage: '<image id="1" src="%s"/><text>This is a simple image toast notification example</text>'
+	})
+	strings = ['Hi from Electron', iconPath]
+  }
+  else if (evt.srcElement.id == "actions") {
+	template = new Template({ 
+	  templateText: '<text>%s</text>',
+      templateImage: '<image id="1" src="%s"/><text>This is a simple actions toast notification example</text>',
+	  templateActions: '<actions><action content="Confirm" arguments="confirm" /><action content="Cancel" arguments="cancel" /></actions>'
+	})
+	strings = ['Hi from Electron', iconPath]
+  }
+  
+  // create the notification object
+  let notification = new Notification({
 		appId: appId,
-		template: new Template({
-      templateText: '<text>%s</text>',
-      templateImage: '<image id="1" src="%s"/><text>This is a simple toast notification example</text>',
-      templateActions: '<actions><action content="Confirm" arguments="confirm" /><action content="Cancel" arguments="cancel" /></actions>'
-    }).getXML(),
-		strings: ['Hi from Electron', iconPath]
-		})
-
-	notification.on('activated', function(t,e) {
-		console.log(e.arguments);
+		template: template.getXML(),
+		strings: strings
+  })
+  
+  // register to notification events
+  notification.on('failed', () => $("#text").text("Notification failed!"))
+  notification.on('dismissed', () => $("#text").text("Notification dismissed!"))
+  
+  // information about the chosen action is provided inside the "arguments" object
+  notification.on('activated', function(t,e) {
     if(e.arguments === 'confirm'){
       $("#text").text("You've clicked Confirm!")
     }
     if(e.arguments === 'cancel'){
       $("#text").text("You've clicked Cancel!")
     }
-	})
-	notification.show()
-  }
+  })
+  
+  // show notification
+  notification.show()
 }
 
 document.addEventListener('DOMContentLoaded', function() {
   document.getElementById("basic").addEventListener("click", doNotify);
+  document.getElementById("image").addEventListener("click", doNotify);
+  document.getElementById("actions").addEventListener("click", doNotify);
 })
