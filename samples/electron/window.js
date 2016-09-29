a6603bf12889cc86e011fceee5f0e1d85ae2a370
Rita Zhang <rita.z.zhang@gmail.com>
2016-09-28 18:22:21 -0700

    :zap: Add sample to update html content with notification action

--- samples/electron/window.js
+++ samples/electron/window.js
@@ -19,21 +19,6 @@ var options = [
 const iconPath = path.join(__dirname, 'icon.png');
 const appId = 'electron-windows-notifications-sample'
 
-template = '<toast launch="app-defined-string">' +
-  '<visual>' + 
-    '<binding template="ToastGeneric">' +
-      '<text>Sample</text>' +
-      '<text>This is a simple toast notification example</text>' +
-      '<image placement="AppLogoOverride" src="' + iconPath  + '" />' +
-    '</binding>' +
-  '</visual>' +
-  '<actions>' +
-    '<action content="check" arguments="check"/>' +
-    '<action content="cancel" arguments="cancel" />' +
-  '</actions>' +
-  '<audio src="ms-winsoundevent:Notification.Reminder"/>' +
-'</toast>';
-
 var basicTemplate = `<toast><visual><binding template="ToastText01"><text id="1">%s</text></binding></visual></toast>`
 var strings = ['Hi!']
 
@@ -44,13 +29,19 @@ function doNotify(evt) {
 		template: new Template({
       templateText: '<text>%s</text>',
       templateImage: '<image id="1" src="%s"/><text>This is a simple toast notification example</text>',
-      templateActions: '<actions><action content="check" arguments="check" /><action content="cancel" arguments="cancel" /></actions>'
+      templateActions: '<actions><action content="Confirm" arguments="confirm" /><action content="Cancel" arguments="cancel" /></actions>'
     }).getXML(),
 		strings: ['Hi from Electron', iconPath]
 		})
 
 	notification.on('activated', function(t,e) {
 		console.log(e.arguments);
+    if(e.arguments === 'confirm'){
+      $("#text").text("You've clicked Confirm!")
+    }
+    if(e.arguments === 'cancel'){
+      $("#text").text("You've clicked Cancel!")
+    }
 	})
 	notification.show()
   }
