f16695ebc2b561d8af9e919b5466832ffa7a06ce
Felix Rieseberg <felix@felixrieseberg.com>
2017-01-19 10:11:52 -0800

    :wrench: Make the linter happy

--- src/toast-notification.js
+++ src/toast-notification.js
@@ -54,9 +54,9 @@ class ToastNotification extends EventEmitter {
     // Do not set this property to true in a toast sent to a Windows 8.x device.
     // Doing so will cause a compiler error or a dropped notification.
 
-    this.notifier = getIsCentennial() ?
-      notifications.ToastNotificationManager.createToastNotifier() :
-      notifications.ToastNotificationManager.createToastNotifier(options.appId)
+    this.notifier = getIsCentennial()
+      ? notifications.ToastNotificationManager.createToastNotifier()
+      : notifications.ToastNotificationManager.createToastNotifier(options.appId)
   }
 
   /**
